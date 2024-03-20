a908d92349cebe6ad2b31b23343a1bc6b737e3f1
Vinicius Euleoterio <vinicius.euleoterio@hotmail.com>
2024-03-20 12:43:59 -0300

    Adicioando endpoints no User

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -8,9 +8,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -18,11 +19,37 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/users")
-    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
+    @GetMapping("/users")
+    public ResponseEntity<List<UserModel>> getUsers() {
+        List<UserModel> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable UUID id) {
+        UserModel user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        boolean deleted = userService.deleteUser(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
+
 }
--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -7,6 +7,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class UserService {
 
@@ -21,5 +25,23 @@ public class UserService {
         userModel = userRepository.save(userModel);
         userProducer.publishMessageEmail(userModel);
         return userModel;
+    }
+
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public UserModel getUserById(UUID id) {
+        Optional<UserModel> userOptional = userRepository.findById(id);
+        return userOptional.orElse(null);
+    }
+
+    public boolean deleteUser(UUID id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
